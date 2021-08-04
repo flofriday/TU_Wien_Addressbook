8fa2e4a778106d33e1722cfaec0d65afd2ba6975
flofriday <flohacksfriday@gmail.com>
2021-08-04 23:50:51 +0200

    Add zoom on profileimages

--- lib/screens/image_screen.dart
+++ lib/screens/image_screen.dart
@@ -11,20 +11,23 @@ class ImageScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: Colors.black,
+        backgroundColor: Color.fromARGB(0xAA, 0x00, 0x00, 0x00),
         elevation: 0,
         title: Text(_title),
       ),
-      body: SafeArea(
-        child: Center(
-          child: Hero(
-            tag: 'personimage',
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(0),
-              child: Image.network(
-                _imageUrl,
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.contain,
+      body: InteractiveViewer(
+        clipBehavior: Clip.none,
+        child: SafeArea(
+          child: Center(
+            child: Hero(
+              tag: 'personimage',
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(0),
+                child: Image.network(
+                  _imageUrl,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.contain,
+                ),
               ),
             ),
           ),
